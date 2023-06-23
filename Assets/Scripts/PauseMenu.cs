0dde5d08cdb4cd8f613aadd2046ea7e0b6aceb13
Merve Akay <merveakayy@users.noreply.github.com>
2023-06-23 19:49:25 +0300

    Initial commit

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+    }
+}
